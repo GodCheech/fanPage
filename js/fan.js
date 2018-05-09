91b779cdd1b1defa3559910f686bc19eae5dc7b7
Danny Ciccarello <Danny@Dannys-MacBook-Pro.local>
2018-05-09 09:46:36 -0400

    have a nice day

---
+++ js/fan.js
@@ -0,0 +1,55 @@
+var numOfPhotos= 13;
+var currentPic= 0;
+
+function hidePhotos(){
+for (var p = 1; p < numOfPhotos; p++) {
+	var photoID = "image" + p;
+	document.getElementById(photoID).style.display = "none";
+
+}
+
+}
+
+
+
+
+
+
+/*function hidePhotos() {
+
+	
+	var numOfPhotos = 4;
+	var current pic = 0;
+
+	for(var p = 1; p < numOfPhotos; p++)
+	{
+		var photoID = "image" + p;
+		document.getElementById(photoID).style.display = "none";
+	}
+}
+*/
+function doRightClick(){
+	var photoID = "image" + currentPic;
+	document.getElementById(photoID).style.display= "none";
+	currentPic = currentPic - 1;
+	if (currentPic <= 0) {
+		currentPic = 10;
+	}
+	photoID = "image" + currentPic;
+	document.getElementById(photoID).style.display = "block";
+}
+
+function doLeftClick(){
+	var photoID = "image" + currentPic;
+	document.getElementById(photoID).style.display = "none";
+	currentPic++;
+	if (currentPic >= numOfPhotos) 
+	{
+		currentPic=0;
+	}
+	
+	photoID = "image" + currentPic;
+	document.getElementById(photoID).style.display = "block";
+}
+
+hidePhotos();
